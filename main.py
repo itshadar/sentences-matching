93a60e00c5fa3f0ef14fbf06c1503489664b46ce
itshadar <hadarber283@gmail.com>
2023-08-13 20:19:42 +0300

    some changing after a great feedback

--- main.py
+++ main.py
@@ -1,17 +1,75 @@
-import argparse
-import os
+from typing import Final, Iterator, TypeAlias
+from collections import namedtuple
+from argparse import Namespace, ArgumentParser
+from pathlib import Path
+
 
 # CONSTANTS
-OUTPUT_FILE = "output.txt"
+OUTPUT_FILE: Final[str] = "output.txt"
 
 # TYPE ALIAS
-Word = tuple[int, str] # tuple of word's index and word's string
-Sentence = list[Word]
+Word = namedtuple("Word", ["word_index", "word_str"])
+Sentence: TypeAlias = list[Word]
 
 
-def parse_args() -> argparse.Namespace:
-    """Return arguments for the program. """
-    parser = argparse.ArgumentParser(description='match pattern to sentences.')
+class Pattern:
+
+    START_INDEX: Final[int] = 2
+
+    def __init__(self, pattern_string, pattern_index):
+        """
+        Initialize a Pattern object with the given pattern string and index.
+
+        :param pattern_string: The string pattern.
+        :param pattern_index: The index of the differing word in the pattern.
+        """
+        self.pattern_string = pattern_string
+        self.pattern_index = pattern_index
+
+    def __eq__(self, other):
+        """
+        Compare if two Pattern objects are equal based on their pattern string and index.
+
+        :param other: The other Pattern object to compare.
+        :return: True if the two Pattern objects are equal, False otherwise.
+        """
+        return (self.pattern_string == other.pattern_string) and (self.pattern_index == other.pattern_index)
+
+    def __hash__(self):
+        return hash((self.pattern_string, self.pattern_index))
+
+
+class PatternCollection:
+
+    def __init__(self):
+        """
+        Initialize an instance that contains patterns and their associated sentences,
+        along with a set of pattern groups - patterns that have more than one associated sentence.
+        """
+        self._data: dict[Pattern, list[Sentence]] = dict()
+        self._pattern_groups: set[Pattern] = set()
+
+    def has_pattern(self, pattern):
+        """Check if a pattern is already found."""
+        return pattern in self._data
+
+    def add_pattern(self, new_pattern: Pattern, pattern_sentence: Sentence):
+        """Add new pattern and initialize its associate sentence."""
+        self._data[new_pattern] = [pattern_sentence]
+
+    def update_pattern(self, pattern: Pattern, pattern_sentence: Sentence):
+        """Update a pattern with additional pattern sentence and define it as a pattern group."""
+        self._data[pattern].append(pattern_sentence)
+        self._pattern_groups.add(pattern)
+
+    def get_patterns_groups(self) -> Iterator[tuple[Pattern, list[Sentence]]]:
+        """Get an iterator over the pattern groups, yielding each pattern and its associated sentences."""
+        return ((pattern, self._data[pattern]) for pattern in self._pattern_groups)
+
+
+def parse_args() -> Namespace:
+    """Return arguments for the program."""
+    parser = ArgumentParser(description='match pattern to sentences.')
     parser.add_argument('-f', '--file', required=True, help='The file path for sentences file.')
     args = parser.parse_args()
     return args
@@ -19,35 +77,37 @@ def parse_args() -> argparse.Namespace:
 
 def convert_to_string(sentence: Sentence) -> str:
     """
-    Covert Sentence type to a simple string.
-    :param sentence: as a Sentence type.
-    :return: sentence as a string.
+    Converts a Sentence type to a simple string.
+
+    :param sentence: A Sentence object.
+    :return: The sentence as a string.
     """
-    return ' '.join([word[1] for word in sentence])
+    return ' '.join([word.word_str for word in sentence])
 
 
 def parse_sentences(content: list[str]) -> list[Sentence]:
     """
-    Parse each simple string sentence to Sentence type object.
-    :param content: list of sentences as a string type
-    :return: list of Sentences type objects
+    Parses each simple string sentence into a Sentence type object.
+
+    :param content: A list of sentences as strings.
+    :return: A list of Sentence type objects.
     """
-    sentences = [[(i, word) for i, word in enumerate(line.strip().split(" "))] for line in content]
+    sentences = [[Word(word_index, word_str) for word_index, word_str in enumerate(line.strip().split(" "))] for
+                 line in content]
     return sentences
 
 
-def read_file(fp: str) -> list[str]:
-    """ Read file. Return lines of file's content. """
+def read_file(fp: Path) -> list[str]:
+    """" Read file. Return lines of file's content. """
     try:
-        with open(fp, 'r') as file:
+        with fp.open() as file:
             return file.readlines()
 
-    except FileNotFoundError as err:
-        print("ERROR:", err.strerror)
-        exit(err.errno)
+    except FileNotFoundError:
+        raise ValueError(f"File not found: {fp}")
 
 
-def write_file(fp: str, content: list[str]) -> None:
+def write_file(fp: Path, content: Iterator[str]) -> None:
     """Write to output file."""
     with open(fp, "w") as output_file:
         output_file.writelines(content)
@@ -55,68 +115,82 @@ def write_file(fp: str, content: list[str]) -> None:
 
 def get_pattern_paragraph(sentences: list[Sentence], pattern_index: int) -> str:
     """
-    Create and return the pattern string paragraph.
-    :param sentences: all the sentences who share the pattern
-    :param pattern_index: The different word's index of the pattern
-    :return: pattern string paragraph.
+    Creates and returns a paragraph containing the pattern string details.
+
+    :param sentences: A list of sentences that share the same pattern.
+    :param pattern_index: The index of the differing word in the pattern.
+    :return: A paragraph containing the detailed pattern string.
     """
-    changing_word = ','.join([sentence[pattern_index][1] for sentence in sentences])
+    changing_word = ','.join([sentence[pattern_index].word_str for sentence in sentences])
     return '\n'.join([convert_to_string(sentence) for sentence in sentences]) + \
            f"\nThe changing word was: {changing_word}\r\n"
 
 
-def sentences_grouping(sentences: list[Sentence]) -> list[str]:
+def extract_pattern_string(sentence: Sentence, pattern_index: int) -> str:
+    """Extracts the pattern string by removing the word at the specified pattern index within the given sentence."""
+    return convert_to_string(sentence[Pattern.START_INDEX:pattern_index]+sentence[pattern_index+1:])
+
+
+def extract_pattern(sentence: Sentence, pattern_index: int) -> Pattern:
+    """Extracts the pattern by specified pattern index within the given sentence."""
+    pattern_string = extract_pattern_string(sentence, pattern_index)
+    return Pattern(pattern_string, pattern_index)
+
+
+def collect_patterns(sentences: list[Sentence]) -> PatternCollection:
     """
-    Grouping sentences who share the same pattern.
-    :param sentences: list of sentences to search for common pattern.
-    :return: list of patterns and the sentences matching it that were found.
+    Collects and extracts all the patterns that exist in the given sentences.
+
+    :param sentences: A list of sentences of the Sentence data type.
+    :return: A PatternCollection instance that holds:
+             1. Each pattern and its associated sentences.
+             2. Patterns that are defined as groups (having more than one associated sentence).
     """
-    sentences_patterns = []
+    patterns_collection = PatternCollection()
+    for sentence in sentences:
 
-    for index, sentence in enumerate(sentences):
+        for pattern_index in range(Pattern.START_INDEX, len(sentence)):
+            pattern = extract_pattern(sentence, pattern_index)
+            if patterns_collection.has_pattern(pattern):
 
-        # Initialize pattern properties - changing pattern index and sentences group.
-        pattern_index = -1
-        pattern_group = [sentence]
+                # update pattern with the additional sentence that found.
+                patterns_collection.update_pattern(pattern, sentence)
+                break
 
-        for sentence_2 in sentences[index+1:]:
+            else:
+                # add new pattern to the patterns collection
+                patterns_collection.add_pattern(pattern, sentence)
 
-            # Get the difference between the sentences pattern
-            diff = list(set(sentence[2:]).difference(sentence_2[2:]))
+    return patterns_collection
 
-            # Check if the difference is matching to a new/exist pattern
-            if len(diff) == 1 and (pattern_index == -1 or diff[0][0] == pattern_index):
 
-                # Save pattern changing word index
-                pattern_index = diff[0][0]
-                pattern_group.append(sentence_2)
-
-                # Each sentence belongs to a single pattern
-                sentences.remove(sentence_2)
-
-        # Check if pattern were found for sentence
-        if pattern_index != -1:
-
-            # Convert pattern group to paragraph format
-            sentences_patterns.append(get_pattern_paragraph(pattern_group, pattern_index))
-
-    return sentences_patterns
+def extract_pattern_groups_output(pattern_collection: PatternCollection) -> Iterator[str]:
+    """Extract from the pattern collection only the patterns that have a group of sentences."""
+    for pattern, sentences in pattern_collection.get_patterns_groups():
+        yield get_pattern_paragraph(sentences, pattern.pattern_index)
 
 
 def main():
 
     # Get needed arguments for program run.
     args = parse_args()
-    input_fp = os.path.abspath(args.file)
-    output_fp = os.path.join(os.path.dirname(input_fp), OUTPUT_FILE)
+    input_fp = Path(args.file)
+    output_fp = input_fp.parent.joinpath(OUTPUT_FILE)
 
     # Read and parse the file content and get sentences patterns matching
-    lines = read_file(input_fp)
+    try:
+        lines = read_file(input_fp)
+
+    except ValueError as e:
+        print(f"Error: {e}")
+        return
+
     sentences = parse_sentences(lines)
-    sentences_groups = sentences_grouping(sentences)
+    patterns_collection = collect_patterns(sentences)
+    output = extract_pattern_groups_output(patterns_collection)
 
     # Write the program output
-    write_file(output_fp, sentences_groups)
+    write_file(output_fp, output)
 
 
 if __name__ == '__main__':
